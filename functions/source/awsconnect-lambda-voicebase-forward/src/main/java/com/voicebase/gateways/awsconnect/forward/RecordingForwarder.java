49d3f1cb7718cacc85055c0b1f70a20a82eb831f
Volker Kueffel <volker@voicebase.com>
2018-05-16 16:57:03 -0700

    Added files I missed before, new packages

--- functions/source/awsconnect-lambda-voicebase-forward/src/main/java/com/voicebase/gateways/awsconnect/forward/RecordingForwarder.java
+++ functions/source/awsconnect-lambda-voicebase-forward/src/main/java/com/voicebase/gateways/awsconnect/forward/RecordingForwarder.java
@@ -1,16 +1,13 @@
 /**
- * Copyright 2016-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License"). You may not
- * use this file except in compliance with the License. A copy of the License is
- * located at 
+ * Copyright 2016-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved. Licensed under the
+ * Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with
+ * the License. A copy of the License is located at
  * 
- *      http://aws.amazon.com/apache2.0/ 
- *      
- * or in the "license" file
- * accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * http://aws.amazon.com/apache2.0/
+ * 
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  *
  */
 package com.voicebase.gateways.awsconnect.forward;
@@ -83,31 +80,42 @@ public class RecordingForwarder {
   public void forwardRequest(Map<String, Object> dataAsMap) {
     try {
 
-      String externalId = dataAsMap.get(Lambda.KEY_EXTERNAL_ID).toString();
+      Object externalId = dataAsMap.get(Lambda.KEY_EXTERNAL_ID);
 
-      MediaProcessingRequestBuilder builder = new MediaProcessingRequestBuilder().withCallbackProvider(callbackProvider)
-          .withConfigureSpeakers(configureSpeakers).withPredictionsEnabled(predictionsEnabled)
-          .withKnowledgeDiscoveryEnabled(knowledgeEnabled).withAdvancedPunctuationEnabled(advancedPunctuationEnabld)
-          .withAwsInputData(dataAsMap).withLeftSpeakerName(leftSpeakerName).withRightSpeakerName(rightSpeakerName);
+      if (externalId != null) {
 
-      MediaProcessingRequest req = builder.build();
+        MediaProcessingRequestBuilder builder = new MediaProcessingRequestBuilder()
+            .withCallbackProvider(callbackProvider).withConfigureSpeakers(configureSpeakers)
+            .withPredictionsEnabled(predictionsEnabled)
+            .withKnowledgeDiscoveryEnabled(knowledgeEnabled)
+            .withAdvancedPunctuationEnabled(advancedPunctuationEnabld).withAwsInputData(dataAsMap)
+            .withLeftSpeakerName(leftSpeakerName).withRightSpeakerName(rightSpeakerName);
 
-      String s3Location = getS3RecordingLocation(dataAsMap);
+        MediaProcessingRequest req = builder.build();
 
-      if (s3Location != null) {
-        String parts[] = s3Location.split("/", 2);
-        String preSignedUrl = createPresignedUrl(parts[0], parts[1], mediaUrlTtl);
-        req.setMediaUrl(preSignedUrl);
+        String s3Location = getS3RecordingLocation(dataAsMap);
+
+        if (s3Location != null) {
+          String parts[] = s3Location.split("/", 2);
+          String preSignedUrl = createPresignedUrl(parts[0], parts[1], mediaUrlTtl);
+          req.setMediaUrl(preSignedUrl);
+        }
+
+        String mediaId =
+            voicebaseClient.uploadMedia(vbApiToken, req, vbApiRetryAttempts, vbApiRetryDelay);
+        if (mediaId != null) {
+          LOGGER.info("Call ID {} sent for processing; mediaId={}", externalId, mediaId);
+        }
+      } else {
+        LOGGER.warn("Received record without contact ID, not a CTR record. Skipping...");
       }
 
-      String mediaId = voicebaseClient.uploadMedia(vbApiToken, req, vbApiRetryAttempts, vbApiRetryDelay);
-      if (mediaId != null) {
-        LOGGER.info("Call ID {} sent for processing; mediaId={}", externalId, mediaId);
-      }
     } catch (SdkClientException | IllegalArgumentException e) {
       LOGGER.warn("Skipping record, unable to generate pre-signed URL.");
     } catch (IOException e) {
       LOGGER.error("Error sending media to VB API", e);
+    } catch (Exception e) {
+      LOGGER.error("Unexpected error", e);
     }
 
   }
@@ -115,24 +123,20 @@ public class RecordingForwarder {
   /**
    * Create a pre-signed URL for given S3 bucket, object key and time to live.
    * 
-   * @param bucketName
-   *          S3 bucket containing the object
-   * @param objectKey
-   *          S3 object key
-   * @param ttl
-   *          time to live for the pre-signed URL.
+   * @param bucketName S3 bucket containing the object
+   * @param objectKey S3 object key
+   * @param ttl time to live for the pre-signed URL.
    * 
    * @return pre-signed URL
    * 
-   * @throws SdkClientException
-   *           if pre-signing the URL failed.
-   * @throws IllegalArgumentException
-   *           if bucket name or object key is empty
+   * @throws SdkClientException if pre-signing the URL failed.
+   * @throws IllegalArgumentException if bucket name or object key is empty
    */
   private String createPresignedUrl(String bucketName, String objectKey, long ttl)
       throws SdkClientException, IllegalArgumentException {
     if (StringUtils.isEmpty(bucketName) || StringUtils.isEmpty(objectKey)) {
-      LOGGER.error("Need bucket and key to create presigned URL. Bucket: {}, key: {}", bucketName, objectKey);
+      LOGGER.error("Need bucket and key to create presigned URL. Bucket: {}, key: {}", bucketName,
+          objectKey);
       throw new IllegalArgumentException("Bucket name or object key missing.");
     }
 
@@ -140,14 +144,16 @@ public class RecordingForwarder {
 
     Date expiration = new Date(msec);
 
-    GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(bucketName, objectKey)
-        .withMethod(HttpMethod.GET).withExpiration(expiration);
+    GeneratePresignedUrlRequest generatePresignedUrlRequest =
+        new GeneratePresignedUrlRequest(bucketName, objectKey).withMethod(HttpMethod.GET)
+            .withExpiration(expiration);
 
     try {
       URL s = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
       return s.toExternalForm();
     } catch (SdkClientException e) {
-      LOGGER.error("Unable to generate pre-signed URL to call recording, bucket {}, key {}.", bucketName, objectKey, e);
+      LOGGER.error("Unable to generate pre-signed URL to call recording, bucket {}, key {}.",
+          bucketName, objectKey, e);
       throw e;
     }
   }
@@ -159,19 +165,27 @@ public class RecordingForwarder {
         Lambda.DEFAULT_ENABLE_KNOWLEDGE_DISCOVERY);
     advancedPunctuationEnabld = getBooleanSetting(env, Lambda.ENV_ENABLE_ADVANCED_PUNCTUATION,
         Lambda.DEFAULT_ENABLE_ADVANCED_PUNCTUATION);
-    leftSpeakerName = getStringSetting(env, Lambda.ENV_LEFT_SPEAKER, Lambda.DEFAULT_LEFT_SPEAKER_NAME);
-    rightSpeakerName = getStringSetting(env, Lambda.ENV_RIGHT_SPEAKER, Lambda.DEFAULT_RIGHT_SPEAKER_NAME);
-    mediaUrlTtl = getLongSetting(env, Lambda.ENV_MEDIA_URL_TTL_MILLIS, Lambda.DEFAULT_MEDIA_URL_TTL_MILLIS);
+    leftSpeakerName =
+        getStringSetting(env, Lambda.ENV_LEFT_SPEAKER, Lambda.DEFAULT_LEFT_SPEAKER_NAME);
+    rightSpeakerName =
+        getStringSetting(env, Lambda.ENV_RIGHT_SPEAKER, Lambda.DEFAULT_RIGHT_SPEAKER_NAME);
+    mediaUrlTtl =
+        getLongSetting(env, Lambda.ENV_MEDIA_URL_TTL_MILLIS, Lambda.DEFAULT_MEDIA_URL_TTL_MILLIS);
 
     vbApiUrl = getStringSetting(env, Lambda.ENV_API_URL, Lambda.DEFAULT_V3_API_URL);
-    vbApiClientLogLevel = getStringSetting(env, Lambda.ENV_API_CLIENT_LOGLEVEL, Lambda.DEFAULT_API_CLIENT_LOG_LEVEL);
+    vbApiClientLogLevel =
+        getStringSetting(env, Lambda.ENV_API_CLIENT_LOGLEVEL, Lambda.DEFAULT_API_CLIENT_LOG_LEVEL);
     vbApiToken = getStringSetting(env, Lambda.ENV_API_TOKEN, null);
-    vbApiRetryDelay = getLongSetting(env, Lambda.ENV_API_RETRY_DELAY, Lambda.DEFAULT_API_RETRY_DELAY);
-    vbApiRetryAttempts = getIntSetting(env, Lambda.ENV_API_RETRY_ATTEMPTS, Lambda.DEFAULT_API_RETRY_ATTEMPTS);
+    vbApiRetryDelay =
+        getLongSetting(env, Lambda.ENV_API_RETRY_DELAY, Lambda.DEFAULT_API_RETRY_DELAY);
+    vbApiRetryAttempts =
+        getIntSetting(env, Lambda.ENV_API_RETRY_ATTEMPTS, Lambda.DEFAULT_API_RETRY_ATTEMPTS);
 
     callbackUrl = getStringSetting(env, Lambda.ENV_CALLBACK_URL, null);
-    callbackMethod = getStringSetting(env, Lambda.ENV_CALLBACK_METHOD, Lambda.DEFAULT_CALLBACK_METHOD);
-    callbackIncludes = getStringListSetting(env, Lambda.ENV_CALLBACK_INCLUDES, Lambda.DEFAULT_CALLBACK_INCLUDES_V3);
+    callbackMethod =
+        getStringSetting(env, Lambda.ENV_CALLBACK_METHOD, Lambda.DEFAULT_CALLBACK_METHOD);
+    callbackIncludes = getStringListSetting(env, Lambda.ENV_CALLBACK_INCLUDES,
+        Lambda.DEFAULT_CALLBACK_INCLUDES_V3);
     additionalCallbackUrls = getStringListSetting(env, Lambda.ENV_CALLBACK_ADDITIONAL_URLS, null);
 
     callbackProvider = new CallbackProvider();
--- functions/source/voicebase-gateway-sdk/src/main/java/com/voicebase/sdk/util/IOUtil.java
+++ functions/source/voicebase-gateway-sdk/src/main/java/com/voicebase/sdk/util/IOUtil.java
@@ -1,24 +1,24 @@
 /**
- * Copyright 2016-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License"). You may not
- * use this file except in compliance with the License. A copy of the License is
- * located at 
+ * Copyright 2016-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved. Licensed under the
+ * Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with
+ * the License. A copy of the License is located at
  * 
- *      http://aws.amazon.com/apache2.0/ 
- *      
- * or in the "license" file
- * accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * http://aws.amazon.com/apache2.0/
+ * 
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  *
  */
 package com.voicebase.sdk.util;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * 
@@ -26,24 +26,41 @@ import java.io.InputStream;
  *
  */
 public class IOUtil {
-	
-	public static File writeToTempFile(InputStream in, String filePrefix) throws IOException {
-		return writeToTempFile(in, filePrefix, null);
-	}
 
-	public static File writeToTempFile(InputStream in, String filePrefix, String suffix) throws IOException {
+  public static File writeToTempFile(InputStream in, String filePrefix) throws IOException {
+    return writeToTempFile(in, filePrefix, null);
+  }
 
-		File file = File.createTempFile(filePrefix, suffix);
+  public static File writeToTempFile(InputStream in, String filePrefix, String suffix)
+      throws IOException {
 
-		try (FileOutputStream f = new FileOutputStream(file)) {
-			byte[] buffer = new byte[4096];
+    File file = File.createTempFile(filePrefix, suffix);
 
-			int bytesRead = 0;
-			while ((bytesRead = in.read(buffer)) != -1) {
-				f.write(buffer, 0, bytesRead);
-			}
-		}
+    try (FileOutputStream out = new FileOutputStream(file)) {
+      copy(in, out);
+    }
 
-		return file;
-	}
+    return file;
+  }
+
+  public static String readToString(InputStream in) throws IOException {
+    String result = null;
+    try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+      copy(in, out);
+      out.flush();
+      result = new String(out.toByteArray(), StandardCharsets.UTF_8);
+    }
+    return result;
+  }
+
+
+  public static void copy(InputStream in, OutputStream out) throws IOException {
+    byte[] buffer = new byte[4096];
+
+    int bytesRead = 0;
+    while ((bytesRead = in.read(buffer)) != -1) {
+      out.write(buffer, 0, bytesRead);
+    }
+  }
+
 }
